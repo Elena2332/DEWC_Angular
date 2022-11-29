1b635cbf72634d3b6bbbc1ef8004dfbcadbfe63f
Elena2332 <elenafesional@gmail.com>
2022-11-29 12:52:34 +0100

    ej1 fin

--- HolaMundo/src/app/components/body/body.component.ts
+++ HolaMundo/src/app/components/body/body.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent {
+  mostrar = false;
+  frase:any = {
+    autor: 'Ben Parker',
+    mensaje: 'Un gran poder conlleva una gran resposavilidad'
+  };
+  personajes = ['Spidermas','Húlk','Thör','Dardevil'];
 
+  cambiarVisibilidad() 
+  {
+    if(this.mostrar)
+      this.mostrar = false; 
+    else
+      this.mostrar = true;
+  }
 }
