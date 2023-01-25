b504d0252771d22dba1d61a3924f38f1c1c1f909
Elena2332 <elenafesional@gmail.com>
2023-01-25 09:13:33 +0100

    Revert "formulario"
    This reverts commit 9bc5739cebc582736676c856451e9be3e4f97585.

--- formulario/src/app/app.module.ts
+++ formulario/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormularioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
